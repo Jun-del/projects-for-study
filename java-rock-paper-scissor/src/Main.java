7650a016d7c2e1d53461d2c67e1b796b9e620805
Jun <neohwj28@gmail.com>
2022-12-31 22:20:47 +0800

    Import libraries

--- java-rock-paper-scissor/src/Main.java
+++ java-rock-paper-scissor/src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
+
