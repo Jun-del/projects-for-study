4b1bab7556603ab3430d09f3bf9eea53eb101ad5
Jun <neohwj28@gmail.com>
2022-12-31 22:25:55 +0800

    Add game window (frame)

--- java-rock-paper-scissor/src/Main.java
+++ java-rock-paper-scissor/src/Main.java
@@ -1,3 +1,4 @@
+// Import libraries
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
@@ -8,3 +9,15 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 
+class Game extends JFrame{
+    public static void main(String[] args){
+        // Create frame (game window)
+        Game game = new Game();
+        game.setTitle("Rock Paper Scissor");
+        game.setBounds(200, 200, 600, 600); // x, y, width, height
+        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        game.getContentPane().setBackground(Color.GRAY);
+        game.setResizable(false);
+        game.setVisible(true);
+    }
+}
