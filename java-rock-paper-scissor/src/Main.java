554bac462929e61a670ac5bc0bce6e9ce70798e3
Jun <neohwj28@gmail.com>
2022-12-31 22:50:16 +0800

    Add rock, paper, scissor icons/buttons and label to the game window

--- java-rock-paper-scissor/src/Main.java
+++ java-rock-paper-scissor/src/Main.java
@@ -10,14 +10,55 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 
 class Game extends JFrame{
+    ImageIcon rock_img, paper_img, scissor_img;
+
+    public void setup(){
+        // Get images
+        rock_img = new ImageIcon(new ImageIcon("src/rock.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+        paper_img = new ImageIcon(new ImageIcon("src/paper.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+        scissor_img = new ImageIcon(new ImageIcon("src/scissor.png").getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
+
+        // Create label
+        // Rock label
+        JLabel rock_label = new JLabel();
+        rock_label.setText("Rock");
+        rock_label.setIcon(rock_img); // Set icon
+        rock_label.setHorizontalTextPosition(JLabel.CENTER);
+        rock_label.setVerticalTextPosition(JLabel.BOTTOM);
+        rock_label.setBounds(150, 300, 100, 100);
+
+        // Paper label
+        JLabel paper_label = new JLabel();
+        paper_label.setText("Paper");
+        paper_label.setIcon(paper_img); // Set icon
+        paper_label.setHorizontalTextPosition(JLabel.CENTER);
+        paper_label.setVerticalTextPosition(JLabel.BOTTOM);
+        paper_label.setBounds(250, 300, 100, 100);
+
+        // Scissor label
+        JLabel scissor_label = new JLabel();
+        scissor_label.setText("Scissor");
+        scissor_label.setIcon(scissor_img); // Set icon
+        scissor_label.setHorizontalTextPosition(JLabel.CENTER);
+        scissor_label.setVerticalTextPosition(JLabel.BOTTOM);
+        scissor_label.setBounds(350, 300, 100, 100);
+
+        // Add components in the frame (game window)
+        add(rock_label);
+        add(paper_label);
+        add(scissor_label);
+        setLayout(null);
+    }
+
     public static void main(String[] args){
         // Create frame (game window)
         Game game = new Game();
-        game.setTitle("Rock Paper Scissor");
+        game.setTitle("Rock Paper Scissor"); // Window title
         game.setBounds(200, 200, 600, 600); // x, y, width, height
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        game.getContentPane().setBackground(Color.GRAY);
-        game.setResizable(false);
+        game.getContentPane().setBackground(Color.WHITE); // Background color
+        game.setup(); // Call setup
+        game.setResizable(false); // Not resizable
         game.setVisible(true);
     }
 }
