f2bcce8e46a0e1ad96d4e01f0adb4c279d5e5b16
Jun <neohwj28@gmail.com>
2023-02-11 15:53:48 +0800

    Add create post page to accept or generate prompt

--- mern-dall-e/src/utils/index.js
+++ mern-dall-e/src/utils/index.js
@@ -1,11 +1,11 @@
 import { surpriseMePrompts } from "../constants";
 
 export function getRandomPrompt(prompt) {
-	const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
-	const randomPrompt = surpriseMePrompts[randomIndex];
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
 
-	// If the random prompt is the same as the current prompt, get a new one
-	if (randomPrompt === prompt) return getRandomPrompt(prompt);
+  // If the random prompt is the same as the current prompt, get a new one
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
-	return randomPrompt;
+  return randomPrompt;
 }
