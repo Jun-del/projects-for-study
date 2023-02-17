7c6fc11945c5e6f576c4b265148c066fe0a55853
Jun <neohwj28@gmail.com>
2023-02-17 21:33:11 +0800

    Add search function

--- mern-dall-e/client/src/pages/Home.jsx
+++ mern-dall-e/client/src/pages/Home.jsx
@@ -15,6 +15,8 @@ const Home = () => {
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -43,6 +45,22 @@ const Home = () => {
     fetchPosts();
   }, []);
 
+  const handleSearchChange = (e) => {
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResults = allPosts.filter(
+          (post) =>
+            post.title.toLowerCase().includes(searchText.toLowerCase()) ||
+            post.prompt.toLowerCase().includes(searchText.toLowerCase()),
+        );
+
+        setSearchResults(searchResults);
+      }, 500),
+    );
+  };
+
   return (
     <section className="max-w-7x1 mx-auto">
       <div>
@@ -56,7 +74,13 @@ const Home = () => {
       </div>
 
       <div className="mt-16 ">
-        <FormField />
+        <FormField
+          labelName="Search Post"
+          type="text"
+          name="text"
+          placeholder="Search Posts"
+          handleChange={handleSearchChange}
+        />
       </div>
 
       <div className="mt-10">
@@ -74,7 +98,7 @@ const Home = () => {
 
             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
               {searchText ? (
-                <RenderCards data={[]} title="No search results found" />
+                <RenderCards data={searchResults} title="No search results found" />
               ) : (
                 <RenderCards data={allPosts} title="No posts found" />
               )}
