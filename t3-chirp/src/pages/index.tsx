d7d7d6beb73fa4225f6f6bad6ccacd30290547b0
Jun <neohwj28@gmail.com>
2023-06-27 15:56:34 +0800

    Post loading state and toast

--- t3-chirp/src/pages/index.tsx
+++ t3-chirp/src/pages/index.tsx
@@ -8,8 +8,9 @@ import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import { toast } from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -25,6 +26,16 @@ const CreatePostWizard = () => {
       setInput("");
 
       void ctx.posts.getAll.invalidate();
+    },
+
+    onError: (err) => {
+      const errorMessage = err.data?.zodError?.fieldErrors.content;
+
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Something went wrong");
+      }
     },
   });
 
@@ -44,9 +55,26 @@ const CreatePostWizard = () => {
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting}
       />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })} disabled={isPosting}>
+          Post
+        </button>
+      )}
+      {isPosting && (
+        <div className="flex items-center justify-center">
+          <LoadingSpinner size={20} />
+        </div>
+      )}
     </div>
   );
 };
@@ -102,7 +130,7 @@ export default function Home() {
   // Starts fetching asap
   api.posts.getAll.useQuery();
 
-  // Return empty div if user isnt loaded
+  // Return empty div if user isn't loaded
   if (!userLoaded) return <div />;
 
   return (
--- t3-chirp/src/server/api/routers/posts.ts
+++ t3-chirp/src/server/api/routers/posts.ts
@@ -25,12 +25,6 @@ const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
   limiter: Ratelimit.slidingWindow(3, "1 m"),
   analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit",
 });
 
 export const postsRouter = createTRPCRouter({
@@ -74,7 +68,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji().min(1).max(280),
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
       })
     )
     .mutation(async ({ ctx, input }) => {
