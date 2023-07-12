8e326a9cc0e690d862dfaf2bf94aa3a03f48fe7e
Jun <neohwj28@gmail.com>
2023-07-13 01:32:57 +0800

    Add subreddit

--- breadit-clone/src/components/UserAccountNav.tsx
+++ breadit-clone/src/components/UserAccountNav.tsx
@@ -49,7 +49,7 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
-          <Link href="/">Create Community</Link>
+          <Link href="/r/create">Create Community</Link>
         </DropdownMenuItem>
 
         <DropdownMenuItem asChild>
