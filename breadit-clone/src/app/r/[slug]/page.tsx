b430c88316917876a122d277d088d2f920f8c84a
Jun <neohwj28@gmail.com>
2023-07-13 17:05:21 +0800

    Add reddit page and create post
    Add subreddit page layout

--- breadit-clone/src/app/r/[slug]/page.tsx
+++ breadit-clone/src/app/r/[slug]/page.tsx
@@ -1,9 +1,50 @@
-import { FC } from "react";
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { INFINITE_SCROLLING_PAGINATION_RESULT } from "@/config";
+import { notFound } from "next/navigation";
+import MiniCreatePost from "@/components/MiniCreatePost";
 
-interface pageProps {}
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+const page = async ({ params }: PageProps) => {
+  const { slug } = params;
+
+  const session = await getAuthSession();
+
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+
+        take: INFINITE_SCROLLING_PAGINATION_RESULT,
+      },
+    },
+  });
+
+  if (!subreddit) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{subreddit.name}
+      </h1>
+
+      <MiniCreatePost session={session} />
+      {/* TODO: show posts in user feed */}
+    </>
+  );
 };
 
 export default page;
