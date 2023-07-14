e634445d4761aee79747d806363bf8f9fb3d3d9c
Jun <neohwj28@gmail.com>
2023-07-14 19:06:51 +0800

    Add create post

--- breadit-clone/src/app/api/subreddit/unsubscribe/route.ts
+++ breadit-clone/src/app/api/subreddit/unsubscribe/route.ts
@@ -12,7 +12,6 @@ export async function POST(req: Request) {
     }
 
     const body = await req.json();
-
     const { subredditId } = SubredditSubscriptionValidator.parse(body);
 
     const subscriptionExists = await db.subscription.findFirst({
