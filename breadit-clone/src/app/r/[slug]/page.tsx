3d4c7f311c4d7e4e2670530f5fe5979f33303520
Jun <neohwj28@gmail.com>
2023-07-15 00:26:21 +0800

    Add infinite scrolling

--- breadit-clone/src/app/r/[slug]/page.tsx
+++ breadit-clone/src/app/r/[slug]/page.tsx
@@ -1,8 +1,9 @@
+import MiniCreatePost from "@/components/MiniCreatePost";
+import PostFeed from "@/components/PostFeed";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
-import { INFINITE_SCROLLING_PAGINATION_RESULT } from "@/config";
 import { notFound } from "next/navigation";
-import MiniCreatePost from "@/components/MiniCreatePost";
 
 interface PageProps {
   params: {
@@ -25,24 +26,23 @@ const page = async ({ params }: PageProps) => {
           comments: true,
           subreddit: true,
         },
-
-        take: INFINITE_SCROLLING_PAGINATION_RESULT,
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
       },
     },
   });
 
-  if (!subreddit) {
-    return notFound();
-  }
+  if (!subreddit) return notFound();
 
   return (
     <>
       <h1 className="font-bold text-3xl md:text-4xl h-14">
         r/{subreddit.name}
       </h1>
-
       <MiniCreatePost session={session} />
-      {/* TODO: show posts in user feed */}
+      <PostFeed initialPosts={subreddit.posts} subredditName={subreddit.name} />
     </>
   );
 };
