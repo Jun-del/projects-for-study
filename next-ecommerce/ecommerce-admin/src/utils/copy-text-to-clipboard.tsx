534ccd11ab0003e00316d30bf4dd9326cc15e648
Jun <neohwj28@gmail.com>
2023-10-14 10:50:01 +0800

    Restructure folders

--- next-ecommerce/ecommerce-admin/src/utils/copy-text-to-clipboard.tsx
+++ next-ecommerce/ecommerce-admin/src/utils/copy-text-to-clipboard.tsx
@@ -4,7 +4,7 @@ export const copyTextToClipboard = async (
   text: string,
   toastMessage?: string
 ): Promise<void> => {
-  if (!navigator?.clipboard) {
+  if (!navigator?.clipboard?.writeText) {
     toast.error(`Failed to copy: Clipboard not supported.`);
     return;
   }
